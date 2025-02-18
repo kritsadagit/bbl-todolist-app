a0381bb1823e0b9ceb34710841456ead77edc391
Kritsada.b <kritsada.buain@hotmail.com>
2025-02-18 15:16:16 +0700

    update UI

---
+++ styles/colors.ts
@@ -0,0 +1,7 @@
+export const PRIMARY_COLOR = "#1e4598"
+export const BACKGROUND_COLOR = "#bbc7e0"
+
+export const DARK_COLOR = "#12295b"
+export const LIGHT_COLOR = "#a5b4d5"
+
+export const LIGHT_COLOR2 = "#d2d9ea"
